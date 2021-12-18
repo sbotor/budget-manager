0c2c6132531f30847312873ed3c07aa3dd6347e5
sbotor <botorszym@gmail.com>
2021-12-18 23:24:13 +0100

    Registered all budget models in the admin site.

--- budgetmanager/budget/admin.py
+++ budgetmanager/budget/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Home)
+admin.site.register(Account)
+admin.site.register(Operation)
+admin.site.register(Label)
